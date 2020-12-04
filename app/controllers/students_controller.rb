a136acba6e828f96bb72f21a2a9788f79fd50747
jose luis <cortezluisjose@gmail.com>
2020-12-04 01:05:15 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,8 @@ class StudentsController < ApplicationController
 
     def set_student
       @student = Student.find(params[:id])
-      
+      @student.active = !@student.active
+    @student.save
+    redirect_to student_path
     end
 end
