ca21265e958b89c684fdfdc1c98b5e48a64a890e
jose luis <cortezluisjose@gmail.com>
2020-12-04 01:21:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :students, only: :index #, as:'students'
-  resources :students, only: :show
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'students/:id/activate', to: 'students#activate', as:'activate-student'
+  
 end
